e74f03b7b1f3d85ce95478c87d9b2196e1b35833
Alexey Ermakov <zee@technocore.ru>
2023-12-01 11:32:11 +0100

    Day 1, part 2

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -1,22 +1,58 @@
+use regex::Regex;
 use utils::aoc_main;
 
-fn part1(input: &str) -> u32 {
-    let lines: Vec<u32> = input
+struct Matchers(Regex, Regex);
+
+fn build_matchers(re: &str) -> Matchers {
+    let first = Regex::new(format!("^.*?({})", re).as_str()).unwrap();
+    let last = Regex::new(format!(".*({}).*?$", re).as_str()).unwrap();
+    Matchers(first, last)
+}
+
+fn parse_digit(token: &str) -> Option<u32> {
+    match token {
+        "1" | "one" => Some(1),
+        "2" | "two" => Some(2),
+        "3" | "three" => Some(3),
+        "4" | "four" => Some(4),
+        "5" | "five" => Some(5),
+        "6" | "six" => Some(6),
+        "7" | "seven" => Some(7),
+        "8" | "eight" => Some(8),
+        "9" | "nine" => Some(9),
+        _ => None,
+    }
+}
+
+fn line_value(line: &str, Matchers(first, last): &Matchers) -> Option<u32> {
+    let first = first.captures(line).and_then(|c| parse_digit(&c[1]));
+    let last = last.captures(line).and_then(|c| parse_digit(&c[1]));
+    Some(first? * 10 + last?)
+}
+
+fn solve(input: &str, matchers: &Matchers) -> u32 {
+    input
         .split('\n')
         .map(|line| {
-            let digits: Vec<u32> = line.chars().flat_map(|c| c.to_digit(10)).collect();
-            digits.first().unwrap_or(&0) * 10 + digits.last().unwrap_or(&0)
+            line_value(line, matchers).unwrap_or_else(|| panic!("No digits found in {}", line))
         })
-        .collect();
-    lines.iter().sum()
+        .sum()
+}
+
+fn part1(input: &str) -> u32 {
+    solve(input, &build_matchers(r"\d"))
 }
 
 fn part2(input: &str) -> u32 {
-    unimplemented!();
+    solve(
+        input,
+        &build_matchers(r"\d|one|two|three|four|five|six|seven|eight|nine"),
+    )
 }
 
 fn main() {
     aoc_main!(part1);
+    aoc_main!(part2);
 }
 
 #[cfg(test)]
@@ -28,13 +64,21 @@ pqr3stu8vwx
 a1b2c3d4e5f
 treb7uchet";
 
+    static EXAMPLE_INPUT_2: &str = "two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen";
+
     #[test]
     fn test_part1() {
         assert_eq!(part1(EXAMPLE_INPUT), 142);
     }
 
-    // #[test]
-    // fn test_part2() {
-    //     assert_eq!(part2(EXAMPLE_INPUT_2), 281);
-    // }
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2(EXAMPLE_INPUT_2), 281);
+    }
 }
